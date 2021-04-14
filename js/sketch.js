f49d4713583bb67899e82f122eac625ebbcd99bc
Tywen <tywen.kelly@gmail.com>
2021-04-13 21:02:13 -0700

    Updated the bad generator

--- js/sketch.js
+++ js/sketch.js
@@ -2,18 +2,22 @@
 let rValueJudg = ['Do', 'Do Not', 'Always', 'Never', 'Sometimes']
 let rVerb = ['Strike', 'Outrun', 'Overthink', 'Forget', 'Remember', 'Listen To', 'Speak Over', 'Forgive', 'Repent', 'Distill', 'Inflate'];
 let rSubj = ['Yourself', 'The Other', 'Your Neighbor', "Them", "Us"];
-let rSubj2 = ['At Dusk', 'Before', 'After']
-let rCon2 = ["Doing", "Feeding", "Feeling", "Giving", "Donating", "Breathing", "Exhaling"]
-let rAdj = ['Good', 'Fast', 'Sad'];
+let rSubj2 = ['Before', 'After', "During", "While"]
+let rCon2 = ["Doing", "Feeding", "Feeling", "Giving", "Donating", "Breathing", "Exhaling", "Living", "Birthing"]
+let rAdv = ['Quickly', 'Sadly', 'Happily', 'Calmly'];
 
 function newQuote() {
     let one = rSelector(rValueJudg);
     let two = rSelector(rVerb);
-    let three = rSelector(rAdj);
+    let three = rSelector(rSubj);
+    let four = rSelector(rSubj2);
+    let five = rSelector(rCon2);
+    let six = rSelector(rAdv);
+
     // console.log(one);
     console.log(one + " " + two + " " + three);
     let item = document.createElement("div");
-    let itemQuote = document.createTextNode(one + " " + two + " " + three);
+    let itemQuote = document.createTextNode(one + " " + two + " " + three + " " + four + " " + five + " " + six);
     item.appendChild(itemQuote);
     document.getElementById("quotes").appendChild(item); 
 }
